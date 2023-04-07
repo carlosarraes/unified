02def1f599dbad704f49af4f3d5a7f12e1ebe4d0
carraes <carraeshb@gmail.com>
2023-04-07 10:50:07 -0300

    feat: create Input component that accepts control and handleChange as props

---
+++ client/src/components/Input.tsx
@@ -0,0 +1,19 @@
+type InputProps = {
+  control: string
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const Input = ({ control, handleChange }: InputProps) => {
+  return (
+    <input
+      name="search"
+      type="text"
+      placeholder="Type here"
+      value={control}
+      onChange={handleChange}
+      className="input input-bordered input-success w-full max-w-md border-sky-900"
+    />
+  )
+}
+
+export default Input
