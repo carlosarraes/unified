927585e357ce6bf8db4394ae30b666f0ecda1ae5
carraes <carraeshb@gmail.com>
2023-04-08 16:28:17 -0300

    feat: Product/SearchQuery struct

--- server/model/dbintmod.go
+++ server/model/dbintmod.go
@@ -20,6 +20,19 @@ type SearchHistory struct {
 	SearchResults []byte `json:"searchResults"`
 }
 
+type Product struct {
+	Title     string  `json:"title"`
+	Price     float64 `json:"price"`
+	Link      string  `json:"permalink"`
+	Thumbnail string  `json:"thumbnail"`
+}
+
+type SearchQuery struct {
+	Web      string `json:"web"`
+	Category string `json:"category"`
+	Query    string `json:"query"`
+}
+
 func OpenDB(dsn string) (*gorm.DB, error) {
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
