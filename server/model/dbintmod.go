e72ba7ec79a4ba17e67b7d746d9c25d4b434cc4d
carraes <carraeshb@gmail.com>
2023-04-08 11:10:40 -0300

    feat: setup connection to db and converts to gorm.DB

---
+++ server/model/dbintmod.go
@@ -0,0 +1,41 @@
+package model
+
+import (
+	"database/sql"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Data interface{}
+
+type MySql struct {
+	*gorm.DB
+}
+
+type SearchHistory struct {
+	gorm.Model
+	Web           string `json:"web"`
+	Category      string `json:"category"`
+	SearchResults []byte `json:"searchResults"`
+}
+
+func OpenDB(dsn string) (*gorm.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	gormDB, err := gorm.Open(mysql.New(mysql.Config{
+		Conn: db,
+	}), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return gormDB, nil
+}
