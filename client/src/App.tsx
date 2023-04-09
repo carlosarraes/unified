80ac158c1feeccd9bea100219231d1109f0af8cf
carraes <carraeshb@gmail.com>
2023-04-09 09:40:50 -0300

    feat: Showcase component receives products from API, App controls products and loading state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Showcase from './components/Showcase'
+import Spinner from './components/Spinner'
+
+const API_URL = 'http://localhost:8080/search'
 
 function App() {
   const [formControls, setFormControls] = useState({
@@ -7,15 +11,41 @@ function App() {
     categorias: '',
     search: '',
   })
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(false)
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    const { name, value } = event.target
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target
     setFormControls({ ...formControls, [name]: value })
   }
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setLoading(true)
+    const { web, categorias } = formControls
+
+    const response = await fetch(`${API_URL}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ web, category: categorias }),
+    })
+    const data = await response.json()
+
+    console.log(data)
+    setProducts(data)
+    setLoading(false)
+  }
+
   return (
-    <main className="flex flex-col justify-center items-center w-screen">
-      <Header formControls={formControls} handleChange={handleInputChange} />
+    <main className="flex flex-col justify-center items-center w-8/12">
+      <Header
+        formControls={formControls}
+        handleChange={handleInputChange}
+        handleSubmit={handleSubmit}
+      />
+      {loading ? <Spinner size={48} /> : <Showcase products={products} />}
     </main>
   )
 }
