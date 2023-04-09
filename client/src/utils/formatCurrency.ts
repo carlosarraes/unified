8db487c0ecac6e9d5890ea81ac144685c4c4d855
carraes <carraeshb@gmail.com>
2023-04-09 09:40:09 -0300

    feat: formatCurrency func to format currency to pt-BR/BRL

---
+++ client/src/utils/formatCurrency.ts
@@ -0,0 +1,7 @@
+export function formatCurrency(currency: number) {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+  }).format(currency)
+}
