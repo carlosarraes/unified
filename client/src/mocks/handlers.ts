a8199dc8ed72ee37c5baaacdffdab9e379d49c51
carraes <carraeshb@gmail.com>
2023-04-10 08:47:34 -0300

    test: setup mock server, so it intercepts the API

---
+++ client/src/mocks/handlers.ts
@@ -0,0 +1,30 @@
+import { rest } from 'msw'
+
+const url = 'https://gouni-4wgfen3n5q-rj.a.run.app/'
+
+const meliMockData = [
+  {
+    title: 'Geladeira',
+    price: 1000,
+    permalink: 'http://geladeira',
+    thumbnail: 'http://geladeira.jpg',
+  },
+]
+
+const buscaMockData = [
+  {
+    title: 'Geladeira',
+    price: 1000,
+    permalink: 'buscapelink',
+    thumbnail: 'data:link',
+  },
+]
+
+export const handlers = [
+  rest.post(url + 'search', (req, res, ctx) => {
+    if (req.body.web === 'meli') {
+      return res(ctx.status(200), ctx.json(meliMockData))
+    }
+    return res(ctx.status(200), ctx.json(buscaMockData))
+  }),
+]
